b27f4d25ac0618fbce408c816634658daee70b05
MANOJ N <MANOJ N@DESKTOP-392F7T5>
2022-12-08 10:09:18 +0530

    frame work

---
+++ Framework/src/GenericScript/Base_Test.java
@@ -0,0 +1,35 @@
+package GenericScript;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class Base_Test implements Framewrok_constsnts {
+	public  WebDriver driver;
+	@BeforeMethod
+	public void openapp()
+	{
+		System.setProperty(gecko_key,gecko_value);
+		 driver=new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+		driver.get(url);
+		
+	}
+@AfterMethod
+public void closeapp(ITestResult result) throws IOException {
+	if(ITestResult.FAILURE==result.getStatus())
+	{
+		genTakeScreen.getphoto(driver);
+	}
+	driver.close();
+}
+	
+	
+	
+	
+}
